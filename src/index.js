83f374ba894b763903a24a458189b75afdce4e5c
JungKata <97729333+JungKata@users.noreply.github.com>
2022-10-11 09:12:07 +0200

    sort fg

--- src/index.js
+++ src/index.js
@@ -20,15 +20,46 @@ document.addEventListener('DOMContentLoaded', function(){
 
     */
 
+    function osszeHasonlit(a, b){
+        if(a < b){
+            return -1;
+        }
+        else if(a > b){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+
+    let t = [ 4, 56 , 715, -99];
+    t.sort((a, b) => {
+        if(a < b){
+            return -1;
+        }
+        else if(a > b){
+            return 1;
+        }else{
+            return 0;
+        }
+    });
+    console.log(t);
+
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Page Loaded');
+    let idozito;
+    //Closure
     document.getElementById('szinezes').addEventListener('click',  ()=> {
        document.body.style.backgroundColor = 'lightblue'; 
         setTimeout(() => {
             document.body.style.backgroundColor = 'white';
         }, 2000);
 
-        setInterval(() => {
+        if(idozito !== undefined){
+            console.log('Clearing timer', idozito)
+            clearInterval(idozito);
+        }
+    
+        idozito = setInterval(() => {
             console.log(document.body.style.backgroundColor);
         },1000);
         
