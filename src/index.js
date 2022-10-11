c803f769a3297df419fc23310f8608ac04cf9b12
JungKata <97729333+JungKata@users.noreply.github.com>
2022-10-11 08:47:42 +0200

    2022.10.11 frontend

---
+++ src/index.js
@@ -0,0 +1,36 @@
+
+
+function fvNeve(param1, param2)
+{
+    
+}
+let masikFv = function(param1, param2){
+    console.log(param1);
+}
+
+/*
+document.addEventListener('DOMContentLoaded', function(){
+    console.log('Page Loaded')
+
+    document.getElementById('szinezes').addEventListener('click', function (){
+        document.body.style.backgroundColor = 'lightblue';
+    });
+    
+    });
+
+    */
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Page Loaded');
+    document.getElementById('szinezes').addEventListener('click',  ()=> {
+       document.body.style.backgroundColor = 'lightblue'; 
+        setTimeout(() => {
+            document.body.style.backgroundColor = 'white';
+        }, 2000);
+
+        setInterval(() => {
+            console.log(document.body.style.backgroundColor);
+        },1000);
+        
+    });
+});
