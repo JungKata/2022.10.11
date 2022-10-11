ef5ae58b5fe4bd54c40097f5a2ce792fa421a159
JungKata <97729333+JungKata@users.noreply.github.com>
2022-10-11 10:33:33 +0200

    nem tiszta fv

--- src/index.js
+++ src/index.js
@@ -33,16 +33,58 @@ document.addEventListener('DOMContentLoaded', function(){
 
     let t = [ 4, 56 , 715, -99];
     t.sort((a, b) => {
-        if(a < b){
+        if(a > b){
             return -1;
         }
-        else if(a > b){
+        else if(a < b){
             return 1;
         }else{
             return 0;
         }
     });
+    t.sort((a, b) => {return a - b});
+    t = t.filter(e => e > Math.abs(e) > 50);
+    t = t.map( e => e*e);
+    t.forEach((e, index) => {
+        console.log(index, e);
+    })
     console.log(t);
+
+
+    //Mellékhatás - side effect
+    //Nincs mellékhatás:
+    1 + 1;
+    Math.sqrt(1 * 2 / 4);
+    [1, 2, 3].filter(e => e > 2);
+    szam > 2;
+    [56, 33].includes(szam);
+    new Kutya();
+    new Date();
+    //Van mellékhatása:
+    console.log('hello');
+    let a = szam++;
+    let b = ++szam;
+    t.push(44);
+    t.sort();
+    console.log(a, b);
+    console.log(t);
+    Math.random();
+
+
+    //Tiszta függvény - pure function:
+    function kettonnelNagyobb(t){
+        return t.filter(e => e > 2);
+    }
+
+    //Nem tiszta console.log() mellekhatás:
+    function tombKiir(t){
+        t.forEach(e => console.log(e));
+    }
+
+    function getButtonColor(){
+        return document.getElementById('szinezes').style.backgroundColor;
+    }
+
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Page Loaded');
